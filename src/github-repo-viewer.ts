ccf4133263bb12948ee3fd7f3148c9898a9b18ff
kito99 <kito-public#virtua.com>
2019-11-15 08:14:42 -0500

    Added filter

--- src/github-repo-viewer.ts
+++ src/github-repo-viewer.ts
@@ -1,7 +1,8 @@
 import {css, customElement, html, LitElement, property} from 'lit-element';
-import {RepositoryResults} from "./github-model";
-import "./repo-card";
-import {card} from "./styles";
+import {RepositoryResults} from './github-model';
+import './repo-card';
+import {card} from './styles';
+import {TopicsChangedEvent} from './repo-filter';
 
 @customElement('virtua-github-repo-viewer')
 export class GithubRepoViewer extends LitElement {
@@ -32,24 +33,64 @@ export class GithubRepoViewer extends LitElement {
     }
 
     results: RepositoryResults | null = null;
+    errorMsg: string | null = null;
 
     private _query: string | null = null;
 
+    connectedCallback() {
+        super.connectedCallback();
+        console.log(this.tagName + ' added to DOM');
+        document.addEventListener('topicsChanged', this.topicsChangedListener);
+    }
+
+    disconnectedCallback() {
+        document.removeEventListener('topicsChange', this.topicsChangedListener);
+    }
+
     protected render() {
         return html`
-        <section class="card">               
-               ${this.results ? this.results.items.map(
-            repository => html`<virtua-repo-card .repository="${repository}"></virtua-repo-card>`) : ''}
-        </section>               
-            `;
+            <section class='card'>                  
+                ${this.errorMsg ? html`Error: ${this.errorMsg}` : null}
+                ${!this.errorMsg ? this.renderCard() : null}
+            </section>               
+        `;
+    }
+
+    private renderCard() {
+        return this.results && this.results.total_count > 0 ? this.results.items.map(repository =>
+                html` 
+                    <virtua-repo-card .repository='${repository}'></virtua-repo-card>          
+                `) : 'No repositories found.';
     }
 
     private async performQuery() {
         if (this.query) {
             const response = await fetch(`${this.url}?q=${this.query}`);
-            this.results = await response.json();
+            if (response.ok) {
+                this.errorMsg = null;
+                this.results = await response.json();
+            } else {
+                this.errorMsg = `${response.status} ${response.statusText}`;
+            }
             this.requestUpdate();
+        } else {
+            this.results = null;
         }
+    }
+
+    // preserve "this"
+    private topicsChangedListener = (event: Event) => this.filter(event);
+
+
+    private filter(event: Event) {
+        const topics = (event as TopicsChangedEvent).detail;
+        let newQuery: string | null = null;
+        if (topics.length > 0) {
+            newQuery = topics.map(topic => 'topic:' + topic).join(',') +
+                '&sort=stars&order=desc&client_id=kmann&client_secret=76d8e82c65b291a6da41e73a7d4deedcee063a24';
+        }
+        console.log('newQuery', newQuery);
+        this.query = newQuery;
     }
 
 }
