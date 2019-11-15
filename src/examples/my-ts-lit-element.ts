c72831a9044aee4fb35733b6b8e1e28bd9817850
kito99 <kito-public#virtua.com>
2019-11-15 12:17:35 -0500

    Added examples

---
+++ src/examples/my-ts-lit-element.ts
@@ -0,0 +1,21 @@
+import {customElement, html, LitElement, property} from 'lit-element';
+
+@customElement('my-ts-lit-element')
+export class MyTsLitElement extends LitElement {
+    @property({type: String}) prop1 = 'Hello World';
+    @property({type: Number}) prop2 = 5;
+    @property({type: Boolean}) prop3 = true;
+    @property({type: Array}) prop4 = [1, 2, 3];
+    @property({type: Object}) prop5 = {subprop1: 'prop 5 subprop1 value'};
+
+    render() {
+        return html`
+      <p>prop1: ${this.prop1}</p>
+      <p>prop2: ${this.prop2}</p>
+      <p>prop3: ${this.prop3}</p>
+      <p>prop4[0]: ${this.prop4[0]}</p>
+      <p>prop5.subprop1: ${this.prop5.subprop1}</p>
+    `;
+    }
+}
+
